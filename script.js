9adf1a36c3d77d39ba78a2e70bbfe75f428f2cb0
ehlwas <myramkenacc@gmail.com>
2024-10-29 08:05:52 +0400

    checkpoint

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+// $(document).ready(function(){
+//     $(".owl-carousel").owlCarousel();
+// });
+
+window.addEventListener("scroll", function (event) {
+    const navigationBar = document.querySelector("#navbar");
+
+    if (window.scrollY > 50) {
+        navigationBar.classList.add("scrolled");
+    } else {
+        navigationBar.classList.remove("scrolled");
+    }
 });
